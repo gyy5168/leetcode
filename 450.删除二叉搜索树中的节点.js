e4465262fce2a7efeb03f73a8fc727c753b0ecb8
guyy <gyy5168@163.com>
2021-02-22 13:52:10 +0800

    BST

---
+++ 450.删除二叉搜索树中的节点.js
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=450 lang=javascript
+ *
+ * [450] 删除二叉搜索树中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+var deleteNode = function(root, key) {
+    if (!root) return null
+    if (root.val === key) {
+      if (!root.left) return root.right
+      if (!root.right) return root.left
+      let rightMinNode = root.right
+      while (rightMinNode.left) {
+        rightMinNode = rightMinNode.left
+      }
+      root.val = rightMinNode.val
+      rightMinNode.val = key
+      root.right = deleteNode(root.right, key)
+      return root
+    } else if (root.val > key) {
+      root.left = deleteNode(root.left, key)
+    } else if (root.val < key) {
+      root.right = deleteNode(root.right, key)
+    }
+    return root
+};
+// @lc code=end
+
