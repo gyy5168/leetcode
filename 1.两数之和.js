7b9724a8a3207dedf29292b868f8c165b6a5697c
guyy <gyy5168@163.com>
2021-03-02 17:11:41 +0800

    two sum

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -11,12 +11,23 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-      let diff = target - nums[i]
-      if (nums.indexOf(diff) > -1 && nums.indexOf(diff) !== i) {
-        return [i, nums.indexOf(diff)]
-      }
-    }
+    // for (let i = 0; i < nums.length; i++) {
+    //   let diff = target - nums[i]
+    //   if (nums.indexOf(diff) > -1 && nums.indexOf(diff) !== i) {
+    //     return [i, nums.indexOf(diff)]
+    //   }
+    // }
+  let len = nums.length
+  let map = new Map()
+
+  for (let i = 0; i < len; i++) {
+    map.set(nums[i], i)
+  }
+
+  for (let i = 0; i < len; i++) {
+    let diff = target - nums[i]
+    if (map.has(diff) && map.get(diff) !== i) return [i, map.get(diff)]
+  }
 };
 // @lc code=end
 
