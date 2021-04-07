c1d08b46240c95e086d5c6ce54286d1668e55fee
guyy <gyy5168@163.com>
2021-04-07 18:19:55 +0800

    longest palindrome

--- 5.最长回文子串.js
+++ 5.最长回文子串.js
@@ -10,45 +10,66 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-  var checkLoop = function (str) {
-    if (str.length < 2) return false
-    var isLoop = true
-    for (let i = 0; i <= str.length / 2; i++) {
-      const ele = str[i]
-      const eleLoop = str[str.length - 1 - i]
-      if (ele !== eleLoop) {
-        isLoop = false
-        break
-      }
+  // var checkLoop = function (str) {
+  //   if (str.length < 2) return false
+  //   var isLoop = true
+  //   for (let i = 0; i <= str.length / 2; i++) {
+  //     const ele = str[i]
+  //     const eleLoop = str[str.length - 1 - i]
+  //     if (ele !== eleLoop) {
+  //       isLoop = false
+  //       break
+  //     }
+  //   }
+  //   return isLoop
+  // }
+  // let group = {}
+  // let loopStr = s[0] || ''
+  // for (let i = 0; i < s.length; i++) {
+  //   const ele = s[i]
+  //   if (group[ele]) {
+  //     group[ele].push(i)
+  //   } else {
+  //     group[ele] = [i]
+  //   }
+  // }
+  // for (const key in group) {
+  //   const arr = group[key]
+  //   if (group.length === 1) continue
+  //   for (let j = 0; j < arr.length - 1; j++) {
+  //     const start = arr[j]
+  //     for (let k = arr.length - 1; k > j; k--) {
+  //       const end = arr[k]
+  //       if (end - start < loopStr.length) break
+  //       var str = s.substring(start, end + 1)
+  //       if (checkLoop(str)) {
+  //         loopStr = str
+  //       }
+  //     }
+  //   }
+  // }
+  // return loopStr
+
+  const n = s.length
+  let res = ''
+
+  const palindrome = (left, right) => {
+    while(left >= 0 && right < n && s[left] === s[right]) {
+      left--
+      right++
     }
-    return isLoop
+    return s.substr(left + 1, right - left - 1)
   }
-  let group = {}
-  let loopStr = s[0] || ''
-  for (let i = 0; i < s.length; i++) {
-    const ele = s[i]
-    if (group[ele]) {
-      group[ele].push(i)
-    } else {
-      group[ele] = [i]
-    }
+
+  for (let i = 0; i < n; i++) {
+    const s1 = palindrome(i, i)
+    const s2 = palindrome(i, i + 1)
+
+    if (s1.length > res.length) res = s1
+    if (s2.length > res.length) res = s2
   }
-  for (const key in group) {
-    const arr = group[key]
-    if (group.length === 1) continue
-    for (let j = 0; j < arr.length - 1; j++) {
-      const start = arr[j]
-      for (let k = arr.length - 1; k > j; k--) {
-        const end = arr[k]
-        if (end - start < loopStr.length) break
-        var str = s.substring(start, end + 1)
-        if (checkLoop(str)) {
-          loopStr = str
-        }
-      }
-    }
-  }
-  return loopStr
+
+  return res
 };
 // @lc code=end
 
